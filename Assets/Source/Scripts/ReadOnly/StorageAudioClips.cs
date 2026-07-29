382c27b342c7fbc35c1adcdd003d8a2e4f02eb46
Antos_Codes <temp88665@gmail.com>
2026-07-29 21:07:09 +0300

    Fix feature
    The method has been restored for your convenience.

--- Assets/Source/Scripts/ReadOnly/StorageAudioClips.cs
+++ Assets/Source/Scripts/ReadOnly/StorageAudioClips.cs
@@ -13,11 +13,23 @@ namespace Game.ReadOnly
         WIND,
         STORM
     }
-    
+
     [CreateAssetMenu(menuName = "Game/Config/Storages/AudioStorages")]
     public class StorageAudioClips : ScriptableObject
     {
         [SerializeField] private SoundList[] soundlist;
+
+#if UNITY_EDITOR
+        private void OnEnable()
+        {
+            string[] names = Enum.GetNames(typeof(SoundType));
+            Array.Resize(ref soundlist, names.Length);
+            for (int i = 0; i < soundlist.Length; i++)
+            {
+                soundlist[i].name = names[i];
+            }
+        }
+#endif
 
         public AudioClip GetRandomClip(SoundType sound)
         {
@@ -26,18 +38,18 @@ namespace Game.ReadOnly
             var clips = soundlist[(int)sound];
             if (clips.Length == 0)
                 return null;
-        
+
             return clips.Get(Random.Range(0, clips.Length));
         }
-        
+
         [Serializable]
         public struct SoundList
         {
+            [HideInInspector] public string name;
             [SerializeField] private AudioClip[] sounds;
-        
+
             public int Length => sounds.Length;
             public AudioClip Get(int indexClip) => sounds[indexClip];
-        
         }
     }
 }
